f30d26a3c94b37e38f3c8bb9b22d56a82f5e1631
vladimirsvsv77 <vladimirsvsv77@gmail.com>
2018-04-24 21:22:56 +0300

    added series

---
+++ srt_to_txt.py
@@ -0,0 +1,58 @@
+import pysrt
+import nltk.data
+import os
+
+
+dir_path = '/Users/vladimie/PycharmProjects/series/'
+qa_file = open('qa_series.txt', 'w')
+
+
+def check_sentense(sent):
+    if len(sent.split()) > 2 and 'swsub.com' not in sent and ']' not in sent and '-->' not in sent \
+            and sent[len(sent) - 1] == '?' and 'переведено' not in sent and '(' not in sent and '<i>' not in sent:
+        return True
+    else:
+        return False
+
+
+def handle_sent(sent):
+    if  sent[0] == '-' or sent[0] == '–':
+        sent = sent[1:len(sent)-1]
+    return sent.replace('\n', ' ').strip() + '\n'
+
+
+def get_qa_from_file(file_parh):
+    try:
+        subs = pysrt.open(file_parh, encoding='cp1251')
+    except UnicodeDecodeError:
+        subs = pysrt.open(file_parh)
+    sub_str = ''
+    for i in subs:
+        sub_str += i.text + ' '
+
+    tokenizer = nltk.data.load('tokenizers/punkt/russian.pickle')
+    sentence = tokenizer.tokenize(sub_str)
+
+    for j in range(len(sentence)):
+        if check_sentense(sentence[j]):
+            try:
+                if sentence[j + 1][len(sentence[j + 1]) - 1] != '?':
+                    qa_file.write(handle_sent(sentence[j]))
+                    qa_file.write(handle_sent(sentence[j + 1]))
+            except IndexError:
+                continue
+
+
+for root, dirs, files in os.walk(dir_path):
+    for file in files:
+        if file.endswith(".srt"):
+            file_parh = os.path.join(root, file)
+            # try:
+            get_qa_from_file(file_parh)
+            # except:
+            #     print(file_parh)
+
+
+
+
+
